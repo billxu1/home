f1669d3c941f4faee3d07c72c2657a8008a8623b
Bill <billxu@unsw.edu.au>
2025-08-02 20:32:00 +1000

    feat: push

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,18 +1,11 @@
-import { Card } from '../stories/components/BaseComponents/card/Card'
-
-const featuredPost = {
-    title: 'Getting Started with React',
-    description: 'Learn the basics of React and how to build your first component.',
-    date: '2024-02-01',
-    image: '/blog-image.jpg',
-    tags: ['React', 'JavaScript', 'Tutorial']
-}
+import { FeaturedPosts } from '../components/home/FeaturedPosts'
+import { RecentPosts } from '../components/home/RecentPosts'
 
 export const Component = () => {
     return (
-        <section className="featured-post">
-            <h2>Featured Post</h2>
-            <Card {...featuredPost} />
-        </section>
+        <>
+            <FeaturedPosts />
+            <RecentPosts />
+        </>
     )
 }
