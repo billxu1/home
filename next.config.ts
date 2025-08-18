fcd4995ffbe9882160078c765bef53004c955c9a
Bill <billxu@unsw.edu.au>
2025-08-18 22:04:18 +1000

    feat: static next js :(

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL('https://media.licdn.com/dms/image/v2/D5603AQH_khTvM8m0GA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1727917902527?e=1758153600&v=beta&t=qoEzYSc_NMSopKPPBeeVBI42ExkqMEVsX4nT1BfG9UY')],
   },
+  output: 'export',
   /* config options here */
 };
 
